4a725a0a97b0d2df6136fb5e354c25aae8e43c31
Abhishek kumar <abhi.arya97161@gmail.com>
2023-12-07 10:11:16 +0530

    some basic routing (/home or /about)

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const http = require("http");
-const { exit } = require("process");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", 'text/html');
-  res.write("<html>")
-  res.write("<body><h1>Welcome to my Node Js project</h1></body>")
-  res.write("</html>")
-  res.end()
+  const url = req.url;
+
+  if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body><h1>Welcome !!!</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(4000);
