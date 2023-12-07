30749ff70dec3a5b3ef5facdc509e4ce997a0fea
Abhishek kumar <abhi.arya97161@gmail.com>
2023-12-07 09:49:52 +0530

    Welcome to my Node Js project

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
+const { exit } = require("process");
 
 const server = http.createServer((req, res) => {
-  console.log("Abhishek kumar");
+  res.setHeader("Content-Type", 'text/html');
+  res.write("<html>")
+  res.write("<body><h1>Welcome to my Node Js project</h1></body>")
+  res.write("</html>")
+  res.end()
 });
 
 server.listen(4000);
